cfda169007915981647aa148af9c11bc9d420524
Zaina Qasim <qasimza@mail.uc.edu>
2024-03-05 03:09:59 -0500

    Added Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="underline">
           Welcome to Rated Rizz!
         </p>
         <a
