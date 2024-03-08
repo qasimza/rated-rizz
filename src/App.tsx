fa04b2a84e164a4d9809b5e045c5fd19531577ac
Zaina Qasim <qasimza@mail.uc.edu>
2024-03-07 22:33:17 -0500

    Button Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Home from './components/home'
 function App() {
   return (
     <div className="">
@@ -10,14 +10,7 @@ function App() {
         <p className="underline text-primary-800 font-bold text-6xl">
           Welcome to Rated Rizz!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Home/>
       </header>
     </div>
   );
---
+++ src/components/home.tsx
@@ -0,0 +1,8 @@
+import Button from '@mui/material/Button';
+export default function Home(){
+    return (
+    <div>
+        <Button>Test Button</Button>
+    </div>
+    )
+}
