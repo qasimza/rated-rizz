6e36273c3d73b49660acac21e7f94fd3c653495b
Zaina Qasim <qasimza@mail.uc.edu>
2024-03-08 01:57:38 -0500

    Added Navbar, search coming soon

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,24 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home'
+import { StyledEngineProvider } from '@mui/styled-engine';
+import { ThemeProvider, createTheme } from '@mui/material';
 
+const muiTheme = createTheme({
+  typography:{
+    fontFamily: ['"Inter"', '"system-ui"'].join(','),
+    }
+  }
+);
 
 function App() {
-  return (
-    <div>
-      <header className="App-header">
-      <Home/>  
-        <p className="underline text-primary-800 font-bold text-6xl">
-          Welcome to Rated Rizz!
-        </p>
-        
-      </header>
-    </div>
+  return (<>
+    <StyledEngineProvider injectFirst>
+    <ThemeProvider theme={muiTheme}>
+      <div><Home/></div>  
+      </ThemeProvider>
+    </StyledEngineProvider>
+  </>
   );
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,18 @@
+import { Box, Typography } from '@mui/material';
 import Button from '@mui/material/Button';
 
 const Navbar = () => {
     return (
-        <div className='w-full h-20 bg-gray-100'>
-            <Button size='large' className='text-primary-800'>Pricing</Button>
-            <Button size='large' className='text-primary-800'>Login</Button>
-            <Button variant='contained' size='large' className='bg-primary-100'>Signup</Button>
+        <div className='flex w-full h-20 bg-gray-100 items-center'>
+            <Box className="bg-primary-700 h-16 w-16 flex items-center justify-center">
+                <Typography className="text-gray-100 font-bold text-5xl">R</Typography>
+            </Box>
+            <Typography className="text-primary-700 font-bold text-4xl ml-2">RatedRizz</Typography>
+            <div className='flex w-[90%] justify-end space-x-5'>
+            <Button size='large' className='text-primary-700 w-32 h-12'>Pricing</Button>
+            <Button size='large' className='text-primary-700 w-32 h-12'>Login</Button>
+            <Button variant='contained' size='large' className='bg-primary-700 w-32 h-12'>Signup!</Button>
+            </div>
         </div>
     )
 }
