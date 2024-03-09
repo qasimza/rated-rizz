026c4e2f07f9bab8ff6121303e66e538d0960586
Zaina Qasim <qasimza@mail.uc.edu>
2024-03-09 13:10:32 -0500

    Search bar component added

---
+++ src/components/LandingPage.tsx
@@ -0,0 +1,12 @@
+import { Container, Typography } from '@mui/material';
+import Navbar from './Navbar';
+import Hero from './Hero';
+
+export default function LandingPage(){
+    return (
+      <>
+        <Navbar/>
+        <Hero/>
+      </>
+    )
+}
