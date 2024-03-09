c4329977ba2d792020cb9ff0e169cb041787323e
Zaina Qasim <qasimza@mail.uc.edu>
2024-03-09 14:48:30 -0500

    Added about pages

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,12 +1,13 @@
-import { Container, Typography } from '@mui/material';
 import Navbar from './Navbar';
 import Hero from './Hero';
+import About from './About';
 
 export default function LandingPage(){
     return (
       <>
         <Navbar/>
         <Hero/>
+        <About/>
       </>
     )
 }
