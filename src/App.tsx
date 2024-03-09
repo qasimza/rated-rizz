814df0b9f2101c385a75c697d92906a9b8bb31e9
Zaina Qasim <qasimza@mail.uc.edu>
2024-03-09 13:17:35 -0500

    removed redundant logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import LandingPage from './components/LandingPage'
 import { StyledEngineProvider } from '@mui/styled-engine';
