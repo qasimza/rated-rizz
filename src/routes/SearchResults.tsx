95f7163063471ab5a24c882d4b583ce862761e48
Zaina Qasim <qasimza@mail.uc.edu>
2024-03-10 04:25:43 -0400

    Added Univeristy Accordian

--- src/routes/SearchResults.tsx
+++ src/routes/SearchResults.tsx
@@ -1,16 +1,80 @@
-import { Stack } from "@mui/material";
+import { Button, Stack, Typography } from "@mui/material";
 import SearchBar from "../shared/components/Searchbar";
 import UnderConstruction from "./errors/UnderConstruction";
+import Accordion from '@mui/material/Accordion';
+import AccordionActions from '@mui/material/AccordionActions';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { ListOfResults, University, getSearchResults } from "../services/search";
+
+const UniversityAccordian = (props: University) => {
+  return (
+    <Accordion
+    className="bg-indigo-100"
+    >
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="panel1-content"
+        id="panel1-header"
+      >
+        <Typography variant="h2" className="text-primary-600 font-bold text-2xl ml-2">
+        {props.name}
+        </Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+      <Typography variant="body1" className="text-xl ml-2">
+        <span className="text-primary-600 font-bold">Location: </span>
+        <span className="text-secondary-900 font-normal">{props.locaiton}</span>
+      </Typography>
+      <Typography variant="body1" className="text-xl ml-2">
+        <span className="text-primary-600 font-bold">Popular Professors: </span>
+        <span className="text-secondary-900 font-normal">{props.top_five}</span>
+      </Typography>
+      <Typography variant="body1" className="text-xl ml-2">
+        <span className="text-primary-600 font-bold">Courses: </span>
+        <span className="text-secondary-900 font-normal">{props.courses}</span>
+      </Typography>  
+      </AccordionDetails>
+        <AccordionActions>
+        <Button className="text-primary-600">Visit University Page</Button>
+     </AccordionActions>
+    </Accordion>
+  )
+}
+
+const ResultList = (props: ListOfResults) => {
+  return (
+    <div>
+        <Stack
+        direction="column"
+        rowGap={3}
+        className="mt-20"
+        >
+        
+        <UniversityAccordian {...props.results[0] as University}/>
+        </Stack>
+    </div>
+  )
+}
+
 
 const SearchResults = () => {
+  const res = getSearchResults()
     return (
         <Stack
         direction="column" 
         justifyContent="center"
         alignItems="center"
-        rowGap={4}
+        rowGap={2}
         >
         <SearchBar/>
+        <Typography variant="h1" className="text-primary-600 font-normal tracking-widest text-xl mt-20">SEARCH RESULTS</Typography>
+        <Typography variant="h2" className="font-bold text-7xl ml-2">
+        <span className="text-secondary-800">Matching </span>
+        <span className="text-primary-600">{res.type}</span>
+        </Typography>
+        <ResultList {...res}/>
       </Stack>
     )
 }
--- src/services/search.ts
+++ src/services/search.ts
@@ -1,3 +1,26 @@
+export type University = {
+    name: String
+    locaiton: String
+    top_five: String[]
+    courses: String[]
+  }
+  
+export type Professor = {
+    name: String
+    university: String
+    courses: String
+    review: String
+  }
+
+const SearchResultTypes = ["Professors", "Universities"] as const;
+export type SearchResultType = typeof SearchResultTypes[number];
+
+  
+export type ListOfResults = {
+    type: SearchResultType;
+    results: University[] | Professor[];
+}
+
 export function addProfessor(){
     return Promise.resolve(true)
 }
@@ -13,9 +36,14 @@ export function searchProfessors(){
     return professors
 }
 
-export function searchUniversities(){
+export function searchUniversities(): University[]{
     const universities = [
-        {label: "University of South Florida"}
+        {
+            name: "University of South Florida",
+            locaiton: "Tampa",
+            top_five: ["Professor One", "Professor Two", "Professor Three", "Professor Four", "Professor Five"],
+            courses: [""]
+        },
     ]
     return universities
 }
@@ -26,6 +54,15 @@ export function getProfessor(){
     }
     
     return professor
+}
+
+export function getSearchResults(): ListOfResults{
+    const universities: University[] = searchUniversities();
+    const searchResults = {
+        type: SearchResultTypes[1],
+        results: universities,
+    }
+    return searchResults
 }
 
 export function getAssignments(){
